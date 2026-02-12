b6eab3894f74aad2affdadd43e396e2d93ed7f76
Salvador González Gerpe <salgonger1997@gmail.com>
2026-02-12 18:24:05 +0000

    added extra staging for stripes

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -9,11 +9,11 @@ source as (
 renamed as (
 
     select
-        id,
-        user_id,
+        id as order_id,
+        user_id as customer_id,
         order_date,
-        status,
-        _etl_loaded_at
+        status as order_status,
+        -- _etl_loaded_at
 
     from source
 
